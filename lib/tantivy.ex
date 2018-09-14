48a2d1507270213b583a1595277d11e8eb2c550d
syfgkjasdkn <kindly@from.network>
2018-09-14 16:48:03 +0000

    init

---
+++ lib/tantivy.ex
@@ -0,0 +1,18 @@
+defmodule Tantivy do
+  @moduledoc """
+  Documentation for Tantivy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tantivy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
